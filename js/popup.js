872d9519ca8a58325a37bda656caeec3e6864d49
jh1202 <86994404+jeon-jinho@users.noreply.github.com>
2022-11-18 09:43:41 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,11 @@
+function ptpopup() {
+    window.open("popup.html", "", "width = 540, height = 455");
+}
+
+function info() {
+    alert("2022 adidas 런 서울 홈페이지 방문 감사드립니다.\n신청기간은 2022-03-07 ~ 2022-03-17 입니다. \n※선착순으로 마감됩니다.")
+}
+
+setTimeout(function () {
+    info();
+}, 1000);
